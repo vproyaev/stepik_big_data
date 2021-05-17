38d92ef99a235af28de3ffee2ce48347d487713b
vlsavanovich <73171654+vlsavanovich@users.noreply.github.com>
2021-05-17 22:55:22 +0300

    Update PySparkJob.py

--- PySparkJob.py
+++ PySparkJob.py
@@ -1,36 +1,26 @@
 import io
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, datediff
+from pyspark.sql.functions import col, sum
 from pyspark.sql import functions as F
 
 
 def process(spark, input_file, target_path):
     df = spark.read.parquet(input_file)
 
-    views = df.filter(df.event == 'view').select('ad_id', 'event').groupBy('ad_id').count().withColumnRenamed('count',
-                                                                                                              'views').withColumnRenamed(
-        'ad_id', 'ad_id0')
-    clicks = df.filter(df.event == 'click').select('ad_id', 'event').groupBy('ad_id').count().withColumnRenamed('count',
-                                                                                                                'clicks').withColumnRenamed(
-        'ad_id', 'ad_id1')
-    ctr = views.join(clicks, views.ad_id0 == clicks.ad_id1, 'outer')
-    ctr = ctr.fillna({'clicks': 0})
-    ctr = ctr.withColumn('CTR', ctr.clicks / ctr.views)
-    ctr = ctr.drop('ad_id1', 'views', 'clicks')
+    ctr = df.groupBy('ad_id').agg((sum(F.when(F.col('event') == 'click', 1).otherwise(0)).alias('click') / sum(F.when(F.col('event') == 'view', 1).otherwise(0)).alias('view')).alias('CTR')).withColumnRenamed('ad_id', 'ad_id1')
+    days = df.select('ad_id', 'date').distinct().groupBy('ad_id').count().withColumnRenamed('count', 'day_count').withColumnRenamed('ad_id', 'ad_id2')
 
-    days = df.select('ad_id', 'date').distinct().groupBy('ad_id').count().withColumnRenamed('count',
-                                                                                            'day_count').withColumnRenamed(
-        'ad_id', 'to_drop')
+    df = df.withColumn('is_cpm', (col('ad_cost_type') == 'CPM').cast('int')).withColumn('is_cpc', (col('ad_cost_type') == 'CPC').cast('int'))
 
-    df = df.withColumn('is_cpm', col('ad_cost_type') == 'CPM')
-    df = df.withColumn('is_cpc', col('ad_cost_type') == 'CPC')
-    df = df.join(days, df.ad_id == days.to_drop, 'outer')
-    df = df.join(ctr, df.ad_id == ctr.ad_id0, 'outer')
-    df = df.drop('to_drop', 'ad_id0')
-    df = df.select('ad_id', 'target_audience_count', 'has_video', df.is_cpm.cast('int'), df.is_cpc.cast('int'),
-                   'ad_cost', 'day_count', 'CTR').distinct().orderBy(F.desc('target_audience_count'))
+    df = df.join(days, df.ad_id == days.ad_id2, 'outer')
+    df = df.join(ctr, df.ad_id == ctr.ad_id1, 'outer')
+    df = df.drop('ad_id1', 'ad_id2')
+
+    df = df.select('ad_id', 'target_audience_count', 'has_video', 'is_cpm', 'is_cpc', 'ad_cost', 'day_count', 'CTR').distinct().orderBy(F.desc('target_audience_count'))
+
     df = df.fillna({'CTR': 0})
+
     df = df.randomSplit([0.75, 0.25])
     df[0].coalesce(4).write.parquet(f'{target_path}/train')
     df[1].coalesce(4).write.parquet(f'{target_path}/test')
